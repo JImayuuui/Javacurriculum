fadcf44df87140b00f6d022bb99412ca0fdfebf4
jimay <jimay@mizuha>
2025-06-16 21:49:34 +0900

    テスト

--- HelloWorld_IijimaYui/src/Sample.java
+++ HelloWorld_IijimaYui/src/Sample.java
@@ -3,7 +3,21 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,world");
-
+		System.out.println("Helloworld{飯島由唯}");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
