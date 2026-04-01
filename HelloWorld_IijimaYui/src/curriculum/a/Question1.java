c7edb089b6a18f46f15cceeafad0012eac5142e3
JImayuuui <jimayui94@gmail.com>
2026-04-01 12:58:16 +0900

    Q1Q2

---
+++ HelloWorld_IijimaYui/src/curriculum/a/Question1.java
@@ -0,0 +1,46 @@
+package curriculum.a;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// Q1. 各型の変数を宣言し、初期値を設定してください。
+		byte byteNum = 0;
+		short shortNum = 0;
+		int intNum = 0;
+		long longNum = 0;
+		float floatNum = 0.0f;		
+		double doubleNum =0.0;
+		char letter = '\u0000';
+		String letters = null;
+		boolean isBoolean = false;
+	//int number;  // int型の変数 number を宣言がこれ基本
+		
+		// O2. 1で宣言した各型の変数に指定された値を代入してください
+		byteNum = 10;
+		shortNum = 100;
+		intNum = 1000;
+		longNum = 10000;		
+		floatNum = 9.5f;
+		doubleNum = 10.5;
+		letter = 'a';
+		letters = "ハロー";
+		isBoolean = true;
+		
+		
+		
+		//結果表示
+		System.out.println("byteNum:" + byteNum);
+		System.out.println("shortNum:" + shortNum);
+		System.out.println("intNum:" + intNum);
+		System.out.println("longNum:" + longNum);
+		System.out.println("floatNum:" + floatNum);
+		System.out.println("doubleNum:" + doubleNum);
+		System.out.println("letter:" + letter);
+		System.out.println("letters:" + letters);
+		System.out.println("isBoolean:" + isBoolean);
+		
+		
+	}
+	}
+
