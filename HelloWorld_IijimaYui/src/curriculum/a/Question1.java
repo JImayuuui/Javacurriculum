cd67926338ddf68a704e852a2d16c2c380e7c102
JImayuuui <jimayui94@gmail.com>
2026-04-02 10:40:41 +0900

    Q4-Q7

--- HelloWorld_IijimaYui/src/curriculum/a/Question1.java
+++ HelloWorld_IijimaYui/src/curriculum/a/Question1.java
@@ -8,7 +8,7 @@ public class Question1 {
 		byte byteNum = 0;
 		short shortNum = 0;
 		int intNum = 0;
-		long longNum = 0;
+		long longNum = 0L;
 		float floatNum = 0.0f;		
 		double doubleNum =0.0;
 		char letter = '\u0000';
@@ -20,7 +20,7 @@ public class Question1 {
 		byteNum = 10;
 		shortNum = 100;
 		intNum = 1000;
-		longNum = 10000;		
+		longNum = 10000L;		
 		floatNum = 9.5f;
 		doubleNum = 10.5;
 		letter = 'a';
@@ -71,40 +71,46 @@ public class Question1 {
 		 System.out.println( byteNum - shortNum );
 		
 		
+		 /*Q4 name という String型の変数 を宣言し
+		 その変数に "山田太郎" という値を代入してください。
+		 name変数を使用してコンソールに こんにちは、山田太郎さん！ と表示してください。
+		*/
 		
+		 String name = "名前";
+		name = "山田太郎";
+		System.out.println( "こんにちは、" + name + "さん！" );
 		
+		//Q5
+		/* age という int型の変数 を宣言し
+         その変数に 25 を代入してください。
+         age変数を使用してコンソールに  年齢: 25歳 という表示してください。
+		*/
 		
+		int age = 25;
+		System.out.println("年齢：" + age +"歳" );
 		
+		/*Q6
+		 num1 という int型の変数 を宣言し、10 を代入
+         num2 という int型の変数 を宣言し、5 を代入
+         num1 と num2 を足した結果を sum という変数に代入し、コンソールに表示してください。
+		*/
 		
+		int num1 = 10;
+		int num2 = 5;
+		int sum = ( num1 + num2 );
+		System.out.println( sum );
 		
+		//Q7 score という int型の変数 を宣言し、80 を代入
+		//score に 20 を加えて、更新する
 		
+		int score = 80;
+		score = score + 20;
+		System.out.println( score );
 		
+		//Q8  price という double型の変数 を宣言し、99.99 を代入
+		//price を int型 に変換し、整数価格: 99 とコンソールに表示してください。
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		double price =99.99;
 		
 		
 		
