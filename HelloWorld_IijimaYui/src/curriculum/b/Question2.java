6e0c1777b06831477b08558a77bac0b0548c49c1
JImayuuui <jimayui94@gmail.com>
2026-04-06 15:57:21 +0900

    curriculmB

--- HelloWorld_IijimaYui/src/curriculum/b/Question2.java
+++ HelloWorld_IijimaYui/src/curriculum/b/Question2.java
@@ -4,7 +4,65 @@ public class Question2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+	//Q1  int 型の変数 score に 75 を代入
+	//  score が 60 以上なら "合格です！" と表示
+	int score = 75;
+	if (score >= 60) {System.out.println("合格です！");}
+		
+	/*Q2  int 型の変数 age に 25 を代入
+		  age が 20 以上 30 以下なら "適正年齢です" と表示
+		 それ以外の場合は "対象外です" と表示
+	 */
+	int age = 25;
+	if (age >= 20 && age <= 30) {
+		System.out.println("適正年齢です");
+		}else {
+		System.out.println("対象外です");
+		}
+		
+	/*Q3 int 型の変数 age に 18 を代入
+		age が 20 以上なら "成人です" と表示
+		age が 13 以上 19 以下なら "ティーンエイジャーです" と表示
+		age が 12 以下なら "子供です" と表示
+	 */
+	int age1 = 18;
+	if (age1 >= 20 ) {
+		System.out.println("成人です");
+		}else if (age1 >= 13 && age1 <= 19) {
+		System.out.println("ティーンエイジャーです");
+		}else if(age1 <= 12){
+		System.out.println("子供です");
+		}
+	/*Q4  int 型の変数 x, y, z に 30, 15, 50 を代入
+		  一番大きい数値を判定して表示
+	 */
+	int x = 30;
+	int y = 15;
+	int z = 50;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
