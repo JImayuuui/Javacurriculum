07e543192183cde700c4112bab5c4223f9344b46
JImayuuui <jimayui94@gmail.com>
2026-04-13 16:59:14 +0900

    Q1

---
+++ HelloWorld_IijimaYui/src/curriculum/b/Question4.java
@@ -0,0 +1,75 @@
+package curriculum.b;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//Q1 int 型の配列 を作成し、1, 2, 3, 4, 5 を格納する
+		//配列の全要素を 順番に表示
+		int[] numbers = {1, 2, 3, 4, 5};
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
