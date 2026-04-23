a4b46b684c0827ea34745b11ae803a6a067c3e2f
JImayuuui <jimayui94@gmail.com>
2026-04-23 18:24:25 +0900

    メソッド

--- HelloWorld_IijimaYui/src/curriculum/b/Main.java
+++ HelloWorld_IijimaYui/src/curriculum/b/Main.java
@@ -7,12 +7,17 @@ public class Main {
 				Greeting myGreeting = new Greeting();
 		    	myGreeting.sayHello();
 		    	
+		 //Q5
+		    	Animal lion = new Animal();
 		    	
+		    	lion.setName("ライオン");
+		        lion.setLength(2.1);
+		        lion.setSpeed(80);
 		    	
-		    	
-		    	
-		    	
-		    	
+		        System.out.println("名前：" + lion.getName());
+		        System.out.println("体長：" + lion.getLength()+ "m");
+		        System.out.println("速度：" + lion.getSpeed()+"km/h");
+		        
 	}
 
 }
