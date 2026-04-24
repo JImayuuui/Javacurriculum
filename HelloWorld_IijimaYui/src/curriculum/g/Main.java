2db32e66b35cee9befb1e9e49327131c5a1caece
JImayuuui <jimayui94@gmail.com>
2026-04-24 18:32:24 +0900

    オブジェクト指向2

---
+++ HelloWorld_IijimaYui/src/curriculum/g/Main.java
@@ -0,0 +1,12 @@
+package curriculum.g;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee woman = new Employee();
+			woman.setEmployeeId("E002");
+			woman.setName("田中花子");				
+			System.out.println("社員ID: " + woman.getEmployeeId() + ", 名前: " + woman.getName());
+				        
+		}
+
+}
