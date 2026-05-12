f53078427591921eb9ae70d3bb6ad56502956ce8
JImayuuui <jimayui94@gmail.com>
2026-05-12 18:53:44 +0900

    K2

--- HelloWorld_IijimaYui/src/curriculum/k/FullTimeEmployee.java
+++ HelloWorld_IijimaYui/src/curriculum/k/FullTimeEmployee.java
@@ -1,5 +1,15 @@
 package curriculum.k;
 
-public class FullTimeEmployee {
+public class FullTimeEmployee extends Employee{
 
+	private static final int HOURLY_RATE = 1200;
+
+	public FullTimeEmployee(String name, int hours) {
+		super(name, hours);
+	}
+
+	@Override
+    public int countPay() {
+        return getHours() * HOURLY_RATE;
+    }
 }
