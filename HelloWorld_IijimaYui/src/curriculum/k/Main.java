d6de1483dd8013c9a37c802d13852647d92752bb
JImayuuui <jimayui94@gmail.com>
2026-05-19 17:11:20 +0900

    K5

--- HelloWorld_IijimaYui/src/curriculum/k/Main.java
+++ HelloWorld_IijimaYui/src/curriculum/k/Main.java
@@ -6,6 +6,16 @@ public class Main {
 		
 		Payable f1 = new FullTimeEmployee("山田", 160);
 		Payable c1 = new ContractEmployee("佐藤", 120);
+	
+		SalaryCalculator calculator = new SalaryCalculator();
+        SalaryReporter reporter = new SalaryReporter();
+
+        int salary1 = calculator.count(f1);
+        int salary2 = calculator.count(c1);
+	
+        reporter.report(f1, salary1);
+        reporter.report(c1, salary2);
+	
 	}
 
 }
